9fa19eef659968a9419aa57c19a0370026e60b48
Jennifer V. <147718561+JnniferV@users.noreply.github.com>
2026-09-17 16:48:18 +0200

    Add files via upload

--- src/DataFixtures/StaffUserFixtures.php
+++ src/DataFixtures/StaffUserFixtures.php
@@ -51,8 +51,6 @@ class StaffUserFixtures extends Fixture implements DependentFixtureInterface
         return [
             ['admin@klask.fr',          'AdminKlask2026!',  AuthorityFixtures::AUTHORITY_ADMIN_REFERENCE,         null],
             ['accompagnateur@klask.fr', 'AccKlask2026!',    AuthorityFixtures::AUTHORITY_ACCOMPANYING_REFERENCE,  'GRP0002'],
-            // boîte du domaine, sert aussi à valider le flux mot de passe oublié
-            ['info@klask.app',          'TestKlask2026!',   AuthorityFixtures::AUTHORITY_ACCOMPANYING_REFERENCE,  'GRP0003'],
         ];
     }
 }
